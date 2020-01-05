4a535a138203ad8595e97f88f100c90aedfcaf6c
Survereignty <survereignty@gmail.com>
2020-01-05 20:46:41 +1100

    fix

--- Debug/CG.cpp
+++ Debug/CG.cpp
@@ -1,11 +1,15 @@
 #include "./bin/CRSC/CRSC.h"
 
-//#include "./bin/Rily/rily.h"
+#include "./bin/Rily/rily.h"
 
 CRSC_Engine E = CRSC_Init("CG", "Corsac");
 SDL_Renderer* r = E.getRenderer();
 CRSC_Graph* g = E.getGraph();
 //Rily r(R);
+
+const int    WALKING_ANIMATION_FRAMES = 4;
+SDL_Rect     gSpriteClips[WALKING_ANIMATION_FRAMES];
+CRSC_Object* gSpriteSheetTexture;
 
 class Menu : public CRSC_Scene
 {
@@ -15,16 +19,36 @@ private:
     CRSC_Object* text;
     CRSC_Timer timer;
     std::stringstream timeText;
-
+    int frame = 0;
     void Loading()
     {
-        background = g->CreateObject("background", 0, 0);
-        foo = g->CreateObject("foo", 240, 190);
+        //background = g->CreateObject("background", 0, 0);
+        SDL_Rect* currentClip = &gSpriteClips[ frame / 4 ];
+        foo = g->CreateObject("foo", ( E.Video.Width - currentClip->w ) / 2,( E.Video.Height - currentClip->h ) / 2, currentClip);
+            gSpriteClips[ 0 ].x =   0;
+            gSpriteClips[ 0 ].y =   0;
+            gSpriteClips[ 0 ].w =  64;
+            gSpriteClips[ 0 ].h = 205;
 
-        timer.start();
-        timeText.str( "" );
-        timeText << "Seconds since start time " << (timer.getTicks() / 1000.f);
-        text = g->CreateText(timeText.str().c_str(), 255, 0, 0, 0, 0);
+            gSpriteClips[ 1 ].x =  64;
+            gSpriteClips[ 1 ].y =   0;
+            gSpriteClips[ 1 ].w =  64;
+            gSpriteClips[ 1 ].h = 205;
+
+            gSpriteClips[ 2 ].x = 128;
+            gSpriteClips[ 2 ].y =   0;
+            gSpriteClips[ 2 ].w =  64;
+            gSpriteClips[ 2 ].h = 205;
+
+            gSpriteClips[ 3 ].x = 196;
+            gSpriteClips[ 3 ].y =   0;
+            gSpriteClips[ 3 ].w =  64;
+            gSpriteClips[ 3 ].h = 205;
+
+        // timer.start();
+        // timeText.str( "" );
+        // timeText << "Seconds since start time " << (timer.getTicks() / 1000.f);
+        // text = g->CreateText(timeText.str().c_str(), 255, 0, 0, 0, 0);
     };
 
     void Events()
@@ -63,6 +87,13 @@ private:
             foo->flip = SDL_FLIP_HORIZONTAL;
         }
         g->DrawingObjects();
+
+        ++frame;
+
+        if( frame / 4 >= WALKING_ANIMATION_FRAMES )
+        {
+            frame = 0;
+        }
 
         SDL_Delay(3);
     }
